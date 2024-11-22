eeaa220f66c9a4e5411b3a6760c294ad40aa60a4
512snigdha <156826817+512snigdha@users.noreply.github.com>
2024-11-22 10:14:22 +0000

    conversion

---
+++ 01_basics/convop.js
@@ -0,0 +1,15 @@
+
+let score = false
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(typeof null);
+
+/* "33" =>33 
+"33bc" => nan
+true=1 false=0*/
+
+let isLoggedIn =1
