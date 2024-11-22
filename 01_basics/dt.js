14a1d2e8e54c17682193d81393a0fd39023a4c2d
512snigdha <156826817+512snigdha@users.noreply.github.com>
2024-11-22 09:12:37 +0000

    datatype and var

---
+++ 01_basics/dt.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+ //alert("Hello") we are using node js not browser. diff syntax is used. code should be readable.
+ // alert documentation....from ecma.script , global object, when to ignore white space.
+
+ console.log(3+3) 
+ console.log("Snigdha")
+
+
+ let name ="Snigdha"
+ let age = 20
+ let isLOggedIn = false
+ let state;
+
+ //number => 2 to the power 53
+ //bigint =. used for large numbers
+ //String=> ""
+ //boolean =true/false
+ //null =>stand alone value/ type / rep empty value
+ //undefined => notassigned till now
+ //symbol =>unique
+
+ //object
+ console.log(typeof "snigdha"); //string
+ console.log(typeof age); // number
+ console.log(typeof null);// object null is an object
+ console.log(typeof undefined); //undefined
+
+
+ 
---
+++ 01_basics/var.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "snigdha45@gmail.com"
+var accountPassword ="16798"
+accountCity = "Odisha"
+let accountState = ""
+let accountPl;
+
+
+//accountId = 2 it will not work coz the value of const cant be change vadeclretion  we can use 2 typs let and var.
+/* const declre in one way. {} =scope. ex it used in if and other condiotional statement. 
+we use let not var in new style. becoz of uswe i block scope... const ansd let use.. var can declare without using datatype or keyword */
+
+
+
+accountEmail = "snigdha2@gmail.com"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountPl]);
